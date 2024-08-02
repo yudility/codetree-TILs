47c9d587be48f0f53b0be1bc4732f033d1bfac33
Yudility <yudility@gmail.com>
2024-08-02 17:30:53 +0900

    Title: 세 수의 중간값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if( b > a && b < c) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    return 0;
+}
