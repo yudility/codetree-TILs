6a89d2272aca9a9777651bfefd09e884296f9599
Yudility <yudility@gmail.com>
2024-08-02 14:34:43 +0900

    Title: 두 실수의 곱, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float a =5.26;
+    float b=8.27;
+
+    printf("%0.3lf\n", round(a*b* 100) / 100);
+
+    return 0;
+}
