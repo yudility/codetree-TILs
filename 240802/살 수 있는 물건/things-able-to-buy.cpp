178e7e253c7f53b6cb6626fd79e1814c51e83f94
Yudility <yudility@gmail.com>
2024-08-02 17:21:46 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n; 
+    cin >> n;
+
+    if(n>=3000) cout << "book";
+    else if( n<3000 && n>=1000) cout << "mask";
+    else cout << "no";
+
+    return 0;
+}
