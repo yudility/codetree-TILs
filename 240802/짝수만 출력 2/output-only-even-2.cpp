227bfe99e9cc3673f4143252fb6ba8ffcce4447e
Yudility <yudility@gmail.com>
2024-08-02 18:13:37 +0900

    Title: 짝수만 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> b >> a;
+
+    while(b>=a){
+        if(b%2==0) cout << b << " ";
+        b--;
+    }
+
+    return 0;
+}
