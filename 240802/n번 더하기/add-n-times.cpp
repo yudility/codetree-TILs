4d9c9bd2363a6ccc8b76a7be3fe970f704fc1a34
Yudility <yudility@gmail.com>
2024-08-02 18:14:56 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/n번 더하기/add-n-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, n;
+
+    cin >> a >> n;
+
+    for(int i=0; i<n; i++){
+        a+=n;
+        cout << a << "\n";
+    }
+
+    return 0;
+}
