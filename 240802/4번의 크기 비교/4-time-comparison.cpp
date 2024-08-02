13d4936076e73412c331dab5726aec06d5fff6ea
Yudility <yudility@gmail.com>
2024-08-02 17:29:37 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b, c, d, e;
+
+    cin >> a;
+    cin >> b >> c >> d >> e;
+
+    if(a>b) cout << 1 <<"\n";
+    else cout << 0 <<"\n";
+
+    if(a>c) cout << 1 <<"\n";
+    else cout << 0 <<"\n";
+
+    if(a>d) cout << 1 <<"\n";
+    else cout << 0 <<"\n";
+
+    if(a>e) cout << 1 <<"\n";
+    else cout << 0 <<"\n";
+
+
+    return 0;
+}
