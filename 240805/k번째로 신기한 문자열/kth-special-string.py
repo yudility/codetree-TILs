5c7ca04bc4c4b12fffd888792e7bb90809079d84
Yudility <yudility@gmail.com>
2024-08-05 04:01:20 +0900

    Title: k번째로 신기한 문자열, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/k번째로 신기한 문자열/kth-special-string.py
+++ 240805/k번째로 신기한 문자열/kth-special-string.py
@@ -4,11 +4,11 @@ a=[]
 
 for i in range(int(arr[0])):
     b=input()
-    if b.startswith('ap'):
+    if b.startswith(arr[2]):
         a.append(b)
     
 
 a=sorted(a)
 
-middle=int(arr[1])-1
+middle=int(arr[1])-1 # middle[46-1]
 print(a[middle])
