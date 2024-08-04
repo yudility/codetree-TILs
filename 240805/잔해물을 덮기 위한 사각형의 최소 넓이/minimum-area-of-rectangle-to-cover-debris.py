65c7e057d8654d6b001d70d5b92663218c5bf6d1
Yudility <yudility@gmail.com>
2024-08-05 05:12:26 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 199ms, Memory: 55MB, Status: Passed - CodeTree

--- 240805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,23 +1,22 @@
-square=[
-    [] for i in range (2)
-]
+fx1, fy1, fx2, fy2 = map(lambda x:x+1000, tuple(map(int,input().split())))
+sx1, sy1, sx2, sy2 = map(lambda x:x+1000, tuple(map(int,input().split())))
+a = [[0]*2000 for _ in range(2001)]
+#첫번쨰 사각형
+for i in range(fx1,fx2):
+    for j in range(fy1,fy2):
+        a[i][j] = 1
+#두번째사각형
+for i in range(sx1,sx2):
+    for j in range(sy1,sy2):
+        a[i][j] = 0
 
-for _ in range (2):
-    x1,y1,x2,y2=map(int,input().split())
-    for i in range (y1,y2):
-        for j in range (x1,x2):
-            square[_].append((j,i))
+min_x, min_y, max_x, max_y = 2001,2001,0,0
+for i in range(fx1,fx2+1):
+    for j in range(fy1,fy2+1):
+        if a[i][j] == 1:
+            min_x = min(min_x,i)
+            min_y = min(min_y,j)
+            max_x = max(max_x,i)
+            max_y = max(max_y,j)
 
-for i in square[1]:
-    for j in square[0]:
-        if j==i:
-            square[0].remove(j)
-
-if square[0]==[]:
-    print(0)
-else:
-    ma=max(square[0])
-    mi=min(square[0])
-    g=ma[0]-mi[0]+1
-    s=ma[1]-mi[1]+1
-    print(g*s)
+print((max_x-min_x+1)*(max_y-min_y+1) if (min_x,min_y,max_x,max_y) != (2001,2001,0,0) else 0)
