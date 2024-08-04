cb692bd8ad2537bceadcfe2ea630425480b3b551
Yudility <yudility@gmail.com>
2024-08-04 21:05:47 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240804/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 100;
+int a[MAX][MAX], n, m;
+int dy[] = {1,0,-1,0};
+int dx[] = {0,1,0,-1};
+
+bool inRange(int y, int x){
+    return (y >= 0 && y < n && x >= 0 && x < n);
+}
+
+bool isComfort(int y, int x){
+    int cnt=0;
+    for(int i=0; i<4; i++){
+        int ny=y+dy[i];
+        int nx=x+dx[i];
+        if(!inRange(ny, nx)) continue;
+        if(a[ny][nx]) cnt++; 
+    }
+
+    return (cnt>=3);
+}
+
+
+int main() {
+
+    cin >> n >> m;
+
+    for(int  i=0; i<m; i++){
+        //칠한다
+        //편안한 상태인지 확인한다
+        int y, x;
+        cin >> y >> x;
+
+        a[y-1][x-1] = 1;
+        cout << isComfort(y-1, x-1) << "\n";
+    }
+
+    return 0;
+}
