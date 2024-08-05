7f2c77afbe2c9997f20c913df03c889f2b861538
Yudility <yudility@gmail.com>
2024-08-06 01:15:46 +0900

    Title: Run Length 인코딩, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/Run Length 인코딩/run-length-encoding.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str[1001];
+    char run_length[1001];
+
+    scanf("%s", str);
+    int n=strlen(str);
+    int idx=0, cnt=1;
+
+    for(int i=0; i<n; i++){
+        if(str[i]==str[i+1]) {
+            cnt++;
+        }
+        else if(str[i]!=str[i+1]){
+            run_length[idx]=str[i];
+            run_length[idx+1]=char(cnt+48);
+            cnt=1;
+            idx += 2;
+        }
+    }
+
+    printf("%d\n%s", strlen(run_length), run_length);
+
+    return 0;
+}
