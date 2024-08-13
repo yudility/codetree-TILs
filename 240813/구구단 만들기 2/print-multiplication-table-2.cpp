b440a83ecf2c5cd80c6ae78830ec2be344cda936
Yudility <yudility@gmail.com>
2024-08-13 15:15:24 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a, b;
+
+    cin >> a >> b;
+
+    for(int i=2; i<=8; i=i+2){
+        for(int j=b; j>=a; j--){
+            cout << j << " * " << i << " = " << j*i << " ";
+            if( j>a){
+                cout << "/ ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
