835944a3fd5120d0e63e34f833940df8ecf75120
Yudility <yudility@gmail.com>
2024-08-13 17:48:29 +0900

    Title: 특정 수와 근접한 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, s, a[100], ret=987654321;
+
+int main() {
+
+    cin >> n >> s;
+
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            int sum=0;
+            for(int k=0; k<n; k++){
+                if(k==i) continue;
+                else if(k==j) continue; 
+                sum+=a[k];
+            }
+            ret = min(ret, abs(s-sum));
+        }
+    }
+
+    cout << ret; 
+
+
+    return 0;
+}
