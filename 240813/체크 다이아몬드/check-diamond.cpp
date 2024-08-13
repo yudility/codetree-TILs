118fcd4a6f21aab3e26e53da986a90d125ee8bbf
Yudility <yudility@gmail.com>
2024-08-13 14:18:59 +0900

    Title: 체크 다이아몬드, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/체크 다이아몬드/check-diamond.cpp
+++ 240813/체크 다이아몬드/check-diamond.cpp
@@ -11,14 +11,12 @@ int main() {
     for(int i=0; i<n; i++){ // 0 1 2 3 4 5 6 7
         for(int j=0; j<n-i-1; j++) cout << " ";  // 3 2 1 0 
         for(int j=0; j<=i; j++ ) cout << "* "; // 1 2 3 4 
-        for(int j=i; j<n-i-1; j++) cout << " ";   // 2 1 0 0 
         
         cout << "\n";
     }
     for(int i=0; i<n-1; i++){
         for(int j=0; j<i+1; j++) cout << " ";   //  0 1 2 
         for(int j=1; j<n-i; j++) cout << "* "; //  3 2 1
-        for(int j=1; j<i; j++) cout << " ";   //  0 1 2
         cout << "\n";
   
     }
