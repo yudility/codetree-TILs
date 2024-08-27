6b4e25b3424f25e64e81332f801fed38e3c1c1a5
Yudility <yudility@gmail.com>
2024-08-27 20:33:42 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240827/삼각형 만들기/create-triangle.cpp
+++ 240827/삼각형 만들기/create-triangle.cpp
@@ -6,7 +6,6 @@ int n, ret, a[100][2];
 
 int main() {
 
-
     cin >> n;
 
     for(int i=0; i<n; i++){
@@ -17,14 +16,18 @@ int main() {
     for(int i=0; i<n; i++){
         for(int j=i+1; j <n; j++){
             for(int k=j+1; k<n; k++){
-                int lx=10000, ly=10000,rx=-10000, ry=-10000;
-                
-                lx=min(a[i][0], min(a[j][0],a[k][0]));
-                ly=min(a[i][1], min(a[j][1],a[k][1]));
-                rx=max(a[i][0], max(a[j][0],a[k][0]));
-                ry=max(a[i][0], max(a[j][1],a[k][1]));
 
-                ret=max(ret, (rx-lx)*(ry-ly));
+                if(a[i][0] !=a[j][0] && a[i][0]!=a[k][0] && a[j][0]!=a[k][0]) continue;
+                if(a[i][1] !=a[j][1] && a[i][1]!=a[k][1] && a[j][1]!=a[k][1]) continue;
+
+                int min_x=10000, min_y=10000,max_x=-10000, max_y=-10000;
+                
+                min_x=min(a[i][0], min(a[j][0],a[k][0]));
+                min_y=min(a[i][1], min(a[j][1],a[k][1]));
+                max_x=max(a[i][0], max(a[j][0],a[k][0]));
+                max_y=max(a[i][0], max(a[j][1],a[k][1]));
+
+                ret=max(ret, (max_x - min_x)*(max_y - min_y));
             }
         }
     }
