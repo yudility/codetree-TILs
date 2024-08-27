4ed00ef6c66cf36674c1436fa2802aa01d562fa9
Yudility <yudility@gmail.com>
2024-08-27 20:41:52 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240827/삼각형 만들기/create-triangle.cpp
+++ 240827/삼각형 만들기/create-triangle.cpp
@@ -14,20 +14,11 @@ int main() {
 
 
     for(int i=0; i<n; i++){
-        for(int j=i+1; j <n; j++){
-            for(int k=j+1; k<n; k++){
-
-                if(a[i][0] !=a[j][0] && a[i][0]!=a[k][0] && a[j][0]!=a[k][0]) continue;
-                if(a[i][1] !=a[j][1] && a[i][1]!=a[k][1] && a[j][1]!=a[k][1]) continue;
-
-                int min_x=10000, min_y=10000,max_x=-10000, max_y=-10000;
-                
-                min_x=min(a[i][0], min(a[j][0],a[k][0]));
-                min_y=min(a[i][1], min(a[j][1],a[k][1]));
-                max_x=max(a[i][0], max(a[j][0],a[k][0]));
-                max_y=max(a[i][0], max(a[j][1],a[k][1]));
-
-                ret=max(ret, (max_x - min_x)*(max_y - min_y));
+        for(int j=0; j <n; j++){
+            for(int k=0; k<n; k++){
+                if(a[i][0]!=a[j][0] || a[i][1] !=a[k][1]) continue;
+        
+                ret=max(ret, abs(a[i][0]-a[k][0])*abs(a[i][1]-a[j][i]));
             }
         }
     }
