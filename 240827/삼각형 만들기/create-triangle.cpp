c5566be8ecbccc55095b34c6b29179a00e20afcd
Yudility <yudility@gmail.com>
2024-08-27 20:51:26 +0900

    Title: 삼각형 만들기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/삼각형 만들기/create-triangle.cpp
+++ 240827/삼각형 만들기/create-triangle.cpp
@@ -12,16 +12,16 @@ int main() {
         cin >> a[i][0] >> a[i][1];
     }
 
-
     for(int i=0; i<n; i++){
         for(int j=0; j <n; j++){
             for(int k=0; k<n; k++){
                 if(a[i][0]!=a[j][0] || a[i][1] !=a[k][1]) continue;
         
-                ret=max(ret, abs(a[i][0]-a[k][0])*abs(a[i][1]-a[j][i]));
+                ret=max(ret, abs(a[i][0]-a[k][0])*abs(a[i][1]-a[j][1]));
             }
         }
     }
+    
     
     cout << ret;
     return 0;
