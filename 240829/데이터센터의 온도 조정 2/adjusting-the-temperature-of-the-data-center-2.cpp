6afc623844cfd451d19b186844bede60d7f3f95e
Yudility <yudility@gmail.com>
2024-08-29 19:57:26 +0900

    Title: 데이터센터의 온도 조정 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240829/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
+++ 240829/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -2,35 +2,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, c, g, h, ta[1000], tb[1000], ret;
+int n, c, g, h, ta[1000], tb[1000], ret, max_t, min_t;
 
 int main() {
 
     cin >> n >> c >> g >> h;
 
-    int max_t = max(c, max(g,h));
-    int min_t = min(c, min(g,h));
 
     for(int i=0; i<n; i++){
         cin>> ta[i] >> tb[i];
+        max_t= max(max_t, tb[i]);
+        min_t= min(min_t, ta[i]);
+        
     }
 
     for(int i=min_t; i<=max_t ; i++){ // i : 현재 온도 
         int sum =0;
+
         for(int j=0; j<n ; j++){ // j : 장비 번호
             if( ta[j] > i ) {
-                sum+= c;
+                sum+=c;
             } 
             else if( ta[j] <= i && tb[j] >= i ){
-                sum+= g;
+                sum+=g;
             }
             else if( tb[j] < i){
-                sum+= h;
+                sum+=h;
             }
         }
 
         ret=max(ret, sum);
-
     }
 
     cout << ret;
