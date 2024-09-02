0aad3e48946bed83a42be1875b4d8d9b6d6de0a2
Yudility <yudility@gmail.com>
2024-09-02 18:33:47 +0900

    Title: 팰린드롬 수 찾기, Time: 55ms, Memory: 0MB, Status: Passed - CodeTree

--- 240902/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
+++ 240902/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
@@ -12,16 +12,19 @@ int main() {
         stack<char> s;
         string ns = to_string(i);
 
-        bool odd =(ns.length()%2 ==1);
-            
-        for(int j=0; j<ns.length(); j++ ){
-            if(odd && j==(ns.length()/2)) continue;
-            
-            if(!s.empty() && s.top()==ns[j]) s.pop();
-            else s.push(ns[j]);
-
+        for(int j=0; j<ns.length()/2; j++){
+            s.push(ns[j]);
         }
+        int center=ns.length()/2;
+        if(ns.length()%2 ==1) center++;
+        
+        for(int j=center; j<ns.length(); j++ ){
+            if(s.top()==ns[j]) s.pop();
+            else break;
+        }
+        
         if(s.size()==0) cnt++;
+    
     }
     cout << cnt;
     
