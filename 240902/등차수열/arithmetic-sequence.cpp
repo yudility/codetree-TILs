8b6183882793e4fe62fa8a1ee49bf33e743ba77e
Yudility <yudility@gmail.com>
2024-09-02 17:22:14 +0900

    Title: 등차수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240902/등차수열/arithmetic-sequence.cpp
+++ 240902/등차수열/arithmetic-sequence.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a[100], cnt;
+int n, a[100],ret;
 
 int main() {
     
@@ -10,21 +10,19 @@ int main() {
 
     for(int i=0; i<n ; i++){
         cin >> a[i];
-
     }
 
-    for(int i=0; i<n; i++){ // ai 정하기
-        for(int j=i+1; j<n; j++){ // aj 정하기
-            int max_n=max(a[i], a[j]);
-            int min_n=min(a[i], a[j]);
-            if(max_n-min_n <= 1) continue;
-            for(int k=min_n; k<max_n; k++){ // 가운데 값 정하기
-                if(a[i]!=k && a[j]!=k && abs(a[i]-k)==abs(k-a[j])) cnt++;
+    for(int k=1; k<=100; k++){ // 가운데 값 정하기      
+        int cnt=0;
+        for(int i=0; i<n; i++){ // ai 정하기
+            for(int j=i+1; j<n; j++){ // aj 정하기
+                if((a[i]+a[j]/2 == k)) cnt++;
             }
         }
+        ret = max(ret, cnt);
     }
 
-    cout << cnt;
+    cout << ret;
 
     return 0;
 }
