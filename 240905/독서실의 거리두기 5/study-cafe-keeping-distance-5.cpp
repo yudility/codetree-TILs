e6da71419b0e547f7b14377337b5d0e721afc932
Yudility <yudility@gmail.com>
2024-09-05 18:36:21 +0900

    Title: 독서실의 거리두기 5, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240905/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240905/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -13,9 +13,10 @@ int main() {
     for(int i=0; i<n; i++){ //i는 사람을 넣을 자리
         int dis=n; //최소 거리
         if(str[i]=='0'){ 
-            int prev=0;
-            for(int j=1; j<n; j++){
-                if(j==i || str[j]=='1') {
+            int prev=-1;
+            for(int j=0; j<n; j++){
+                if(prev==-1 && str[j] =='1') prev=j;
+                else if(j==i || str[j]=='1') {
                     dis = min(dis,(j-prev));
                     prev=j;
                 }
