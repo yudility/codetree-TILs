dce0e1dd1178fd089599d5c0be0f6252db1d72e1
Yudility <yudility@gmail.com>
2024-09-05 18:54:15 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240905/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240905/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -5,30 +5,35 @@ using namespace std;
 int n, ret;
 string str;
 
+int get_dis(){
+    int dis=n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n;  j++){
+            if( str[i]=='1' && str[j]=='1'){
+                dis = min(dis, j-i);
+            }
+        }
+    }
+    return dis;
+}
+
 int main() {
 
     cin >> n;
     cin >> str;
 
     for(int i=0; i<n; i++){ //i는 사람을 넣을 자리
-        int dis=n; //최소 거리
+        if(str[i]=='0'){
 
-        if(str[i]=='0'){ 
-            int prev;
-            for(int j=0; j<n; j++){
-                if(str[j]=='1'||j==i){
-                    prev=j; //처음 1이 나온 위치 저장
-                    break;
-                }
-            }
-            for(int j=0; j<n; j++){ // 처음부터 돌면서 거리 계산
-                if((j==i || str[j]=='1' )&& j!=prev) {
-                    dis = min(dis,(j-prev));
-                    prev=j;
-                }
-            }
-            ret = max(ret, dis);
+            str[i]='1';
+
+            ret = max(ret, get_dis());
+
+            str[i]='0';
+
         }
+        
     }
 
     cout << ret;
