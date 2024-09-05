a969ef14f792f2cd780b1ce9fe4e520da8cf6167
Yudility <yudility@gmail.com>
2024-09-05 14:19:42 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240905/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
+++ 240905/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ret;
+int n, ret=1;
 string str;
 
 
@@ -16,7 +16,7 @@ int main() {
         bool t = false; //두번 나오는지 검사
 
         for(int j=0; j<=n-i; j++){ // j부터 i까지의 길이 문자열 검사할 것
-            for(int k= j+1; k<n-i; k++){ //k부터 i길이의 부분 문자열이 완전 같을 경우 true
+            for(int k= j+1; k<=n-i; k++){ //k부터 i길이의 부분 문자열이 완전 같을 경우 true
                 bool issame = true;
 
                 for(int l=0; l< i; l++){//i 길이 만큼 검사 시작
@@ -32,7 +32,7 @@ int main() {
     
     }
 
-    cout << ret+1;
+    cout << ret;
 
     
     return 0;
