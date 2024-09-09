cdef11501f4fde4ba2bde4c189375e2111b33350
Yudility <yudility@gmail.com>
2024-09-09 17:17:03 +0900

    Title: 초기 수열 복원하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240909/초기 수열 복원하기/restore-initial-sequence.cpp
+++ 240909/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -2,21 +2,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, s[1001];
-vector<int> v;
-
-bool isSeq(){
-    for(int i=0; i<n-1; i++){
-        if(s[i]!=(v[i]+v[i+1])) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void printSeq(vector<int> &v){
-    for(int i=0; i<n; i++) cout << v[i] << " ";
-}
+int n, s[1002], used[1002];
+//a 정답, v 저장소
+//s는 입력 받을 인접한 원소의 합
 
 int main() {
 
@@ -25,19 +13,32 @@ int main() {
     for(int i=0; i<n-1; i++){
         cin >> s[i];
     }
-    
-    for(int i=1; i<n+1; i++){
+
+    vector<int> v;
+    for(int i=1; i<=n; i++){
+        memset(used, 0, sizeof(used));
+        v.clear();
         v.push_back(i);
+        used[i]=1;
+
+        for(int j=1; j<n; j++){
+            v.push_back(s[j-1]-v[j-1]);
+            used[s[j-1]-v[j-1]]++;
+        }
+
+        bool flag=true;
+        for(int j=1; j<=n; j++){
+            if(used[j]>1){
+                flag=false;
+                break;
+            }
+        }
+        if(flag) break;
     }
 
-    do{
-        if(isSeq()){
-            printSeq(v);
-            break;
-        }
-    }while(next_permutation(v.begin(), v.end()));
-
-    
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] <<" ";
+    }
     
     return 0;
 }
