4aae9c10214af8eaa4299bfde87aad99bebe7cd2
Yudility <yudility@gmail.com>
2024-09-12 18:38:44 +0900

    Title: 달리자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240912/달리자/run-and-run.cpp
+++ 240912/달리자/run-and-run.cpp
@@ -19,19 +19,8 @@ int main() {
     
     for(int i=0; i<n; i++){
         if(a[i]>b[i]){
-            int movingPeople=a[i]-b[i];
-            for(int j=i+1; j<n; j++){
-                if(a[j]<b[j] && (movingPeople+a[j])<=b[j]  ){
-                    a[j]=a[j]+movingPeople;
-                    cnt+=(movingPeople*(j-i));
-                    break;
-                }else if(a[j]<b[j]&& (movingPeople+a[j])>b[j] ){
-                    a[i+1]=a[j]+movingPeople-b[j]; //남은 인원 옆으로 보내기
-                    a[j]=b[j];
-                    cnt+=(movingPeople*(j-i));
-                    break;
-                }
-            }
+            a[i+1]+=(a[i]-b[i]);
+            cnt+=(a[i]-b[i]);
         }
     }
     
