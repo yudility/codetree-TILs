d7ea4603ef16f15a7008a5dd227a8fdbf7ce7622
Yudility <yudility@gmail.com>
2024-09-13 18:13:07 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240913/코딩톡/coding-talk.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, u[100], p, visited[26];
+char c[100];
+
+int main() {
+
+    cin >> n >> m >> p;
+
+    for(int i=0; i<m; i++){
+        cin >> c[i] >> u[i];
+    }
+
+    if(u[p-1]>0){
+        for(int i=p-1; i<m; i++){
+            visited[(int)(c[i]-65)]++;
+        }
+
+        for(int i=0; i<n; i++){
+            if(visited[i]==0){
+                cout<< (char)(i+65) <<" ";
+            }
+        }
+    }
+
+
+
+    return 0;
+}
