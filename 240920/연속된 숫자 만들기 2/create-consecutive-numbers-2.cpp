585adb58f08f2bcf7ec8f1cb46d7d6700fd70903
Yudility <yudility@gmail.com>
2024-09-20 17:51:05 +0900

    Title: 연속된 숫자 만들기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240920/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[3], d1, d2;
+
+int main() {
+
+    for(int i=0; i<3; i++){
+        cin >> a[i];
+    }
+
+    sort(a, a+3);
+
+    d1=a[1]-a[0];
+    d2=a[2]-a[1];
+
+    if(d1==0 && d2==0) cout << 0;
+    else if(d1==2 || d2==2) cout << 1;
+    else cout << 2;
+
+    return 0;
+}
