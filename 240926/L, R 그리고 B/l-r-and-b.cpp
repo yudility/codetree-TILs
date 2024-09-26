811dd027385179ff409f6b86ca7db954bbbbb113
Yudility <yudility@gmail.com>
2024-09-26 13:28:13 +0900

    Title: L, R 그리고 B, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240926/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+char m[10][10];
+int lx, ly, bx, by, rx, ry, ret;
+
+
+int main() {
+
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            cin >> m[i][j];
+            if(m[i][j]=='B'){
+                bx=j;
+                by=i;
+            }
+            if(m[i][j]=='L'){
+                lx=j;
+                ly=i;
+            } 
+            if(m[i][j]=='R'){
+                rx=j;
+                ry=i;
+            }  
+        }
+    }
+
+    ret = abs(lx-bx)+abs(ly-by)-1; 
+
+    cout << ret;
+
+
+    return 0;
+}
