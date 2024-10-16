4bbce44c578176133261a76fbd970655405761c9
Yudility <yudility@gmail.com>
2024-10-16 15:08:43 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241016/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double b=0.165;
+
+        System.out.printf(a + " * "+"%.6f"+ " = "+ "%.6f",b, a*b);
+    }
+}
