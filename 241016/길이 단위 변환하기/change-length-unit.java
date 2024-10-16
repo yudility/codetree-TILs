c257948f8cf6a7ad61d05716afdf09bde46053cf
Yudility <yudility@gmail.com>
2024-10-16 15:12:30 +0900

    Title: 길이 단위 변환하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241016/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double p = 9.2;
+        double m = 1.3;
+
+        System.out.printf(p+"ft = "+"%.1fcm\n",p*30.48 );
+        System.out.printf(m+"mi = "+"%.1fcm",m*160934 );
+    }
+}
