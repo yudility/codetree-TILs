8518654bcdd17b4238a75fb4d6fa4c211a47d6db
Yudility <yudility@gmail.com>
2024-11-02 12:22:45 +0900

    Title: Date to Date, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

--- 241102/Date to Date/date-to-date.py
+++ 241102/Date to Date/date-to-date.py
@@ -3,7 +3,7 @@ m1, d1, m2, d2 = map(int, input().split())
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 if m1 == m2:
-    print(d2-d1)
+    print(d2-d1+1)
 else:
     days = (num_of_days[m1] - d1)
 
